c313a3226874f2de3ac90cc50ec61f01d97d40d7
MelinP <exelon10@gmail.com>
2021-12-24 19:13:50 +0200

    Practice class

---
+++ src/main/java/Switch.java
@@ -0,0 +1,33 @@
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.GeckoDriverService;
+//import org.openqa.selenium.opera.OperaDriver;
+//
+//public class Switch {
+//
+//    if WebDriver == driver;public void Run(){
+//        driver = new ChromeDriver();
+//        driver = new GeckoDriverService();
+//        driver = new OperaDriver();
+//        driver.manage().window().maximize();
+//
+//        UseDriver = "Chrome";
+//        switch(UseDriver){
+//            case "Chrome":
+//                System.out.println("UseDriver = Chrome ");
+//                System.setProperty("web-driver.chrome.driver", "./drivers/chromedriver");
+//                break;
+//            case "Opera":
+//                System.out.println("UseDriver = Opera");
+//                System.setProperty("web-driver.chrome.driver", "./drivers/operadriver");
+//                break;
+////            case "FireFox":
+////                System.out.println("UseDriver + Firefox");
+////                System.setProperty("web-driver.chrome.driver", "./drivers/geckodriver");
+////                break;
+//            default:
+//                System.out.println("Driver is not valid = " + UseDriver);
+//        }
+//
+//
+//}
--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,13 +1,16 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.security.PublicKey;
+import org.openqa.selenium.firefox.GeckoDriverInfo;
+import org.openqa.selenium.firefox.GeckoDriverService;
+import org.openqa.selenium.opera.OperaDriver;
 
 public class TestClass {
 
     public WebDriver driver;
+    public String UseDriver;
 
     @Before
     public void Run(){
@@ -17,6 +20,7 @@ public class TestClass {
         driver.manage().window().maximize();
 
     }
+
     @Test
     public void sendEmailAndPasswordTest(){
         String email = "sojaki2262@idrct.com";
@@ -26,19 +30,19 @@ public class TestClass {
         LoginPage.openLoginPage().CleaEmailAndPassword().shortAuth(email, password);
     }
 
-    @Test
-    public void openUserSettingsRedactAndSubmit(){
-        String firstName = "FuriousDoggo";
-        String lastName = "SeriousDoggo";
-        UserSettingsClass SettingsClass = new UserSettingsClass(driver);
-
-        SettingsClass.openSettingsPage().clearFids().shortMethod(firstName, lastName);
-    }
-
-    @Test
-    public void TestCombo(){
-        this.sendEmailAndPasswordTest();
-        this.openUserSettingsRedactAndSubmit();
-    }
+//    @Test
+//    public void openUserSettingsRedactAndSubmit(){
+//        String firstName = "FuriousDoggo";
+//        String lastName = "SeriousDoggo";
+//        UserSettingsClass SettingsClass = new UserSettingsClass(driver);
+//
+//        SettingsClass.openSettingsPage().clearFids().shortMethod(firstName, lastName);
+//    }
+//
+//    @Test
+//    public void TestCombo(){
+//        this.sendEmailAndPasswordTest();
+//        this.openUserSettingsRedactAndSubmit();
+//    }
 
 }
